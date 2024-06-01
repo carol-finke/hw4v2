4038dfafc7f3d4f51f5d75bf42083f042404c7fb
Carol Finke <carol.finke@kellogg.northwestern.edu>
2024-06-01 16:03:15 +0000

    updates jun 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   get("/", { :controller => "places", :action => "index" })
-  resources "entries"
-  resources "places"
-  resources "sessions"
-  resources "users"
+  resources :entries
+  resources :places
+  resources :sessions
+  resources :users
+
+  # Login/logout
+  get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
+
+  # Define the root route
+  root 'places#index'
 end

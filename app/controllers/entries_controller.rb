278211d5b8937135a3fa6a9c63d09d840228b663
Carol Finke <carol.finke@kellogg.northwestern.edu>
2024-06-02 18:58:29 +0000

    workingon june 2

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,19 +2,24 @@ class EntriesController < ApplicationController
   before_action :require_user
 
   def new
+    @place = Place.find(params[:place_id])
+    @entry = @place.entries.build
   end
 
   def create
-    @entry = Entry.new
-    @entry.title = params[:title]
-    @entry.description = params[:description]
-    @entry.occurred_on = params[:occurred_on]
-    @entry.place_id = params[:place_id]
+    @place = Place.find(params[:place_id])
+    @entry = @place.entries.build(entry_params)
     @entry.user_id = @current_user.id
     if @entry.save
-      redirect_to place_path(@entry.place_id)
+      redirect_to place_path(@place)
     else
       render :new
     end
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:title, :description, :occurred_on)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root 'places#index'
-  
-  get 'signup', to: 'users#new'
-  get 'login', to: 'sessions#new'
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
   resources :users, only: [:new, :create]
   resources :places do
-    resources :entries, only: [:new, :create, :index, :show]
+    resources :entries, only: [:index, :new, :create, :show]
   end
 end
